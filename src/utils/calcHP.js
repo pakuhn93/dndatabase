bdbf4f9674e19e380b69090dd6742cd9e93f2f07
pakuhn93 <pakuhn93@gmail.com>
2025-03-21 18:00:28 -0700

    added notes for what to create next in calcHP, which is a function that will calculate the total HP for a character

---
+++ src/utils/calcHP.js
@@ -0,0 +1,25 @@
+const calcHP = () => {
+    // Below are the things that will be needed for this to work.
+    // Level 1 Class HP
+        // LET baseClass variable
+        // CONST array to hold the HP_AVG of each class
+    // Constitution modifier
+        // LET modCON variable
+    // Total Level
+        // LET levelTotal
+    // Tough Feat
+        // LET featTough T/F
+    // Draconic Sorcerer
+        // LET isSorcererDraconic T/F
+    // Dwarf
+        // LET isDwarf T/F
+
+        
+    // Extra Features
+        // take in a numerical value
+
+
+    // FUTURE: Multiclass T/F in the form
+}
+
+export { calcHP };
